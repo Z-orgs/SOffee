1e44e0ef9b2d50f20c2ff7d9d50e169c0b5b2752
drmerveem <nsovipgl@gmail.com>
2022-10-30 23:46:11 +0700

    uncmt

--- src/config/app.js
+++ src/config/app.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
 require("dotenv/config");
+const shortid_1 = __importDefault(require("shortid"));
 function configApp(app) {
     app.use((0, express_fileupload_1.default)());
     app.use(express_1.default.static('./src/public'));
@@ -14,7 +15,7 @@ function configApp(app) {
     app.set('views', './src/view');
     app.use(express_1.default.urlencoded({ extended: false }));
     app.use(express_1.default.json());
-    // app.use(cookieParser(shortid.generate()));
-    app.use((0, cookie_parser_1.default)('a'));
+    app.use((0, cookie_parser_1.default)(shortid_1.default.generate()));
+    // app.use(cookieParser('a'));
 }
 exports.default = configApp;
